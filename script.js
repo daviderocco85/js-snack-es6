3680376641bb70078cedc19ecbf9e329df3b772e
daviderocco85 <roccodavide85@gmail.com>
2026-06-09 00:10:43 +0200

    feat: implement solution for Snack 2 in script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-// Snack 1
+// ===============================
+// SNACK 1
+// ===============================
+
 const raceBikes = [
     {
         name: 'Atala',
@@ -48,3 +51,166 @@ for (const bike of raceBikes) {
 }
 
 console.log(lightest);
+
+// ===============================
+// SNACK 2
+// ===============================
+
+const footballTeams = [
+
+    {
+         name: 'Atalanta',
+        'league points': 0,
+        'fouls against': 0
+    },
+
+    {
+         name: 'Bologna',
+        'league points': 0,
+        'fouls against': 0
+    },
+
+    {
+         name: 'Cagliari',
+        'league points': 0,
+        'fouls against': 0
+    },
+
+    {
+         name: 'Como',
+        'league points': 0,
+        'fouls against': 0
+    },
+
+    {
+         name: 'Fiorentina',
+        'league points': 0,
+        'fouls against': 0
+    },
+
+    { 
+         name: 'Frosinone',
+        'league points': 0,
+        'fouls against': 0
+    },
+     
+    {
+         name: 'Genoa',
+        'league points': 0,
+        'fouls against': 0
+    },
+  
+    {
+         name: 'Inter',
+        'league points': 0,
+        'fouls against': 0
+    },
+
+    {
+         name: 'Juventus',
+        'league points': 0,
+        'fouls against': 0
+    },
+
+    {
+         name: 'Lazio',
+        'league points': 0,
+        'fouls against': 0
+    },
+
+    {
+         name: 'Lecce',
+        'league points': 0,
+        'fouls against': 0
+    },
+
+    {
+         name: 'Milan',
+        'league points': 0,
+        'fouls against': 0
+    },
+
+    {
+         name: 'Monza',
+        'league points': 0,
+        'fouls against': 0
+    },
+
+    {
+         name: 'Napoli',
+        'league points': 0,
+        'fouls against': 0
+    },
+
+    {
+         name: 'Parma',
+        'league points': 0,
+        'fouls against': 0
+    },
+
+    {
+         name: 'Roma',
+        'league points': 0,
+        'fouls against': 0
+    },
+
+    {
+         name: 'Sassuolo',
+        'league points': 0,
+        'fouls against': 0
+    },
+
+    {
+         name: 'Torino',
+        'league points': 0,
+        'fouls against': 0
+    },
+
+    {
+         name: 'Udinese',
+        'league points': 0,
+        'fouls against': 0
+    },
+
+    {
+         name: 'Venezia',
+        'league points': 0,
+        'fouls against': 0
+    }
+
+
+
+];
+
+ // intervallo tra 0 e 114 che esprime il numero massimo 
+ // di punti che una squadra può fare in 38 giornate.
+/** Arrow function che genera numeri random per i punti fatti*/ 
+const getRandomNumberPoints = () => Math.floor(Math.random() * 115);
+
+
+// si stima che una squadra possa subire 
+// almeno 100 falli nell'arco di una stagione
+/**Arrow function che genera numeri random per i falli subiti*/
+const getRandomNumberFouls = () => Math.floor(Math.random() * 1000) + 100;
+
+// Ciclo for per aggiungere i numeri randomici all'array di oggetti footballTeams
+for (let i = 0 ; i < footballTeams.length; i++){
+
+    footballTeams[i]['league points'] = getRandomNumberPoints();
+    footballTeams[i]['fouls against'] = getRandomNumberFouls();
+   
+}
+
+
+// Creazione del nuovo array di oggetti con solo Nome squadra e falli subiti
+const teamsNameAndFouls = [];
+
+// Ciclo for che li aggiunge nell'array di oggetti teamsNameAndFouls 
+for (let i = 0; i < footballTeams.length; i++) {
+  teamsNameAndFouls.push({
+    name: footballTeams[i].name,
+    'fouls against': footballTeams[i]['fouls against']
+  });
+}
+
+console.log(teamsNameAndFouls);
